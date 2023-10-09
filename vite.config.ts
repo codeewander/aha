19cbc8ef7c5dfb02fee1614b23e081e3e59a1ae8
Kira <changru.yang02@gmail.com>
2023-10-09 22:14:36 +0800

    feat: modify deployment setting

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
-  build: {
-    rollupOptions: {
-      input: 'src/index.tsx',
-    },
-  },
 });
